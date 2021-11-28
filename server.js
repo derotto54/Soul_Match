d0c729478896234df22237ccf8e449eb72e91926
LinhBui88 <87442413+Linhbui88@users.noreply.github.com>
2021-11-27 21:26:37 -0500

    first commit optimizing from Sould_Match

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ const sequelize = require('./config/connection');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const app = express();
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3002;
 
 const hbs = exphbs.create({});
 
